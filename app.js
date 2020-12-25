932a9f8061ed0278f7faf0f7445bd796feb6735c
adalbertopc <adalbertinho90@gmail.com>
2020-12-25 12:36:33 -0700

    fixed: nav-bar in mobile.  added:border-top to footer and decreased padding

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const navButton = document.querySelector('.nav__button');
 const navLinks = document.querySelector('.nav-links');
 const navButtonIco = document.querySelector('.nav__button__ico');
@@ -5,7 +6,9 @@ navButton.addEventListener('click', () => {
 	navLinks.classList.toggle('show');
 	if (navButtonIco.innerHTML === 'menu') {
 		navButtonIco.innerHTML = 'clear';
+		body.style.overflowY = 'hidden';
 	} else {
 		navButtonIco.innerHTML = 'menu';
+		body.style.overflowY = 'auto';
 	}
 });
